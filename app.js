6641bfd717e253f4ebc549547d6a54d7707858a4
OEM Configuration (temporary user) <salej1@hotmail.com>
2017-09-08 01:50:54 -0500

    v0.4 changed port to 8080

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
+var PORT = process.argv[2] || 3000
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT + '!');
 });
