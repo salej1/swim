852f638cb7932d8175637e17f74651a30ab46e1e
OEM Configuration (temporary user) <salej1@hotmail.com>
2017-11-15 02:29:51 -0600

    v1.4.1 length fix

--- public/js/video.js
+++ public/js/video.js
@@ -1,6 +1,6 @@
 var video = function video(containerId, options) {
 	var html = '<video width="600" autoplay>\
-<source src="' + options[Math.round(Math.random() * options.length-1)].src + '" type="video/mp4">\
+<source src="' + options[Math.round(Math.random() * (options.length - 1))].src + '" type="video/mp4">\
 </video>';
 
 	var container = $('#' + containerId);
